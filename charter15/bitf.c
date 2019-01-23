99a91962f86ea3b5d85260fc846b96b6d961a4c2
= <=>
2019-01-23 23:58:11 +0800

    添加 C 位操作测试

--- charter15/bitf.c
+++ charter15/bitf.c
@@ -23,16 +23,38 @@ int main(void) {
     if ( (myNum & ste_num_c) == ste_num_c) {
         printf(" \n The value is ste_num_c! \n");
     }
+    
+    //置位功能(把某一位设置为1) 
+    //置位后值为 19
+    int bit_1 = 0x12;
+    bit_1 |= 0x01;
+    printf("\n bits :%d \n",bit_1);
 
-    //置位功能
-    int 
-    STEP(myNum);
+    //某一位清零 bit_2 值为 31
+    int bit_2 = 0x1f; //bit_2 值为 31
+    bit_2 &= 0x1c;
+    printf("\n bit_2:%d \n",bit_2); //值为 28
+
+    //位翻转(如果原来bit为1则经过运算后这个值为0，1~~>0,0~~>1)
+    //对于^符号, 0^b = b, 保持不变
+    int bit_3 = 0x09;
+    bit_3 ^= 0x0b;
+    printf("\n bit_3 :%d \n",bit_3); // 值为 2
+
+    //右移位操作
+    unsigned int bit_4 = 0xa;
+    printf(" \n originValue:%d,offset:%d \n",bit_4,bit_4 >> 2); //0xa,2,
+
+    unsigned int bit_5 = 0x1c;
+    bit_5 >>= 2;
+    printf("\n bit_5 :%d \n",bit_5); //  7
+
+    //左移位操作
+    unsigned int bit_6 = 0x3;
+    printf("\n originValue:%d,offsetValue:%d",bit_6,bit_6<<2);// 3,12
+
+    unsigned int bit_7 = 0x11;
+    bit_7 <<= 2;
+    printf("\n the value is %d \n",bit_7); //17, 68 
     return 0;
 }
-
-#ifdef __cplusplus
-    extern "C" {
-        //c 函数的生命
-        //或者包含头文件
-    }
-#endif 
